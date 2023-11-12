11aff1a3a0d766e5391fe0094718bb423024fdff
Yadhukrishna <145049379+yadhukrishnx@users.noreply.github.com>
2023-11-12 08:45:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+
+    $(document).ready(function(){
+        $('.dropdown-toggle').dropdown();
+    });
+
+  function color(id){
+    
+    document.getElementById(id).style.background="rgba(233, 127, 7, 0.37)";
+  }
+  function decolor(){
+    
+    document.getElementById("topnav").style.background=" rgba(77, 139, 64, 0.27)";
+  }
+  function at(){
+    confirm("Confirm to Contact ? ")
+   
+  }
+  function service(){
+    alert("Contact 9746585950 for Getting Services . Thank You");
+  }
